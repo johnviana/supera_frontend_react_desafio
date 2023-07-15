3f526df292353ad24057bbc5997c37c23facff6f
wedsysa <johnvianaluz@hotmail.com>
2023-07-15 16:08:13 -0300

    feat: funcao fetchdata criada, para realizar a chamanda da api e os dados vindo dela com base no filtro

--- desafioo-supera-react/src/Formulario.js
+++ desafioo-supera-react/src/Formulario.js
@@ -11,5 +11,35 @@ function Formulario () {
     const [saldoTotal, setSaldoTotal] = useState(0);
     const [saldoPorPeriodo, setSaldoPorPeriodo] = useState(0);
 
+    const fetchData = async () => {
+        const url = 'http://localhost:8080/api/transferencias';
+    
+        const params = {};
+        
+        if (dataInicio) {
+          const dataInicioFormatada = moment(dataInicio, 'DD/MM/YYYY').format('YYYY-MM-DD');
+          params.startDate = dataInicioFormatada;
+        }
+        if (dataFim) {
+          const dataFimFormatada = moment(dataFim, 'DD/MM/YYYY').format('YYYY-MM-DD');
+          params.endDate = dataFimFormatada;
+        }
+        if (operador) {
+          params.nomeOperadorTransacao = operador;
+        }
+    
+        try {
+          const response = await axios.get(url, { params });
+          const data = response.data.map(transferencia => {
+            const { dataTransferencia } = transferencia;
+            const dataTransferenciaFormatada = moment(dataTransferencia).format('DD/MM/YYYY');
+            return { ...transferencia, dataTransferencia: dataTransferenciaFormatada };
+          });
+          setTransferencias(data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
 }
 export default Formulario;
