4c9a53683d3267b12525924b66630629c4990140
wedsysa <johnvianaluz@hotmail.com>
2023-07-15 16:02:33 -0300

    feat: useState criados para o form

--- desafioo-supera-react/src/Formulario.js
+++ desafioo-supera-react/src/Formulario.js
@@ -4,7 +4,12 @@ import './App.css';
 import moment from 'moment';
 
 function Formulario () {
-   
+    const [dataInicio, setDataInicio] = useState('');
+    const [dataFim, setDataFim] = useState('');
+    const [operador, setOperador] = useState('');
+    const [transferencias, setTransferencias] = useState([]);
+    const [saldoTotal, setSaldoTotal] = useState(0);
+    const [saldoPorPeriodo, setSaldoPorPeriodo] = useState(0);
 
 }
 export default Formulario;
